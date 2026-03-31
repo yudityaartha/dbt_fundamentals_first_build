c3e3e9382963f8abd433cb3e1ed8a34abe3554b6
yudityaartha <yuditya_artha@outlook.com>
2026-03-31 17:36:13 +0700

    typos on dbt_project.yml and models/scratch.sql

---
+++ models/scratch.sql
@@ -0,0 +1,6 @@
+{% set cool_string  = 'Hello, World!' %}
+{% set second_cool_string  = 'this is jinja' %}
+{% set my_fav  = 'its a great language' %}
+{% set dur  = 1 %}
+
+{{ cool_string }} {{ second_cool_string }} {{ my_fav }}. I want to learn it for {{ dur }} month
